f8d11a3f8547f0e7096ff46cb01681f2f4fb5325
Sam <samuel.e.leonard@gmail.com>
2018-06-18 21:59:43 -0400

    Woking on links to different divs

--- app/assets/scripts/ToggleVW.js
+++ app/assets/scripts/ToggleVW.js
@@ -1,54 +1,56 @@
+let sectionContHome = document.querySelector('#section-container-home');
+let sectionContAbout = document.querySelector('#section-container-about');
+let sectionContProjects = document.querySelector('#section-container-projects');
+let sectionContResume = document.querySelector('#section-container-resume');
+let sectionContContact = document.querySelector('#section-container-contact');
 let home = document.querySelector('#home');
 let homeSection = document.querySelector('#home-section');
 let about = document.querySelector('#about');
+let aboutSection = document.querySelector('#about-section');
 let projects = document.querySelector('#projects');
+let projectsSection = document.querySelector('#projects-section');
 let resume = document.querySelector('#resume');
+let resumeSection = document.querySelector('#resume-section');
 let contact = document.querySelector('#contact');
+let contactSection = document.querySelector('#contact-section');
 
-  home.addEventListener('click', function() {
-    console.log('clicked');
-    homeSection.classList.toggle('home--hidden');
-    homeSection.classList.toggle('home');
-    about.classList.toggle('about');
-    about.style.visibility = 'hidden';
-    projects.classList.toggle('projects');
-    projects.style.visibility = 'hidden';
-    resume.classList.toggle('resume');
-    resume.style.visibility = 'hidden';
-    contact.classList.toggle('contact');
-    contact.style.visibility = 'hidden';
+about.addEventListener ('click', function() {
+
+  if (aboutSection.classList.contains('.hidden')) {
+      aboutSection.classList.remove('.hidden');
+    } else {
+        console.log('alala');
+    }
+
+  if (sectionContAbout.classList.contains('.hidden')) {
+      sectionContAbout.classList.remove('.hidden');
+    } else {
+        console.log('lololo');
+  }
+
+  if (homeSection.classList.contains('.hidden')) {
+      console.log('home contains hidden')
+    } else {
+        homeSection.classList.add('.hidden');
+    }
+
+    if (sectionContHome.classList.contains('.hidden')) {
+        console.log('home contains hidden')
+      } else {
+          sectionContHome.classList.add('.hidden');
+      }
+
+
   });
 
-    about.addEventListener('click', function() {
-    home.classList.toggle('home');
-    home.style.visibility = 'hidden';
-    about.classList.toggle('about');
-    about.style.visibility = 'visi';
-    projects.classList.toggle('projects');
-    projects.style.visibility = 'hidden';
-    resume.classList.toggle('resume');
-    resume.style.visibility = 'hidden';
-    contact.classList.toggle('contact');
-    contact.style.visibility = 'hidden';
+home.addEventListener ('click', function() {
+
+    if (homeSection.classList.contains('hidden')) {
+      homeSection.classList.remove('hidden');
+      aboutSection.classList.add('hidden');
+      sectionContAbout.classList.add('hidden');
+      sectionContHome.classList.remove('hidden');
+    } else {
+        console.log('alala');
+    }
   });
-
-
-
-
-
-
-
-
-
-
-//let home = document.querySelector('#home');
-
-//  home.addEventListener('click', function() {
-//    var home = document.getElementById("home");
-//    if (home.style.display === "none") {
-//      home.style.display = "block";
-//    } else {
-//      home.style.display = "none";
-//      console.log('aaa');
-//    }
-//  });
