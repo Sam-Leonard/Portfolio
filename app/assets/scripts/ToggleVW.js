9b4eccbb1fe35e02c675f33ea0c252e8320dcd4c
Sam <samuel.e.leonard@gmail.com>
2018-06-30 22:07:31 -0400

    changed to only hide classes if loaded on larger screen, still needs resize function, started on about section small screen size.

--- app/assets/scripts/ToggleVW.js
+++ app/assets/scripts/ToggleVW.js
@@ -8,6 +8,20 @@ let projects = document.querySelector('#projects');
 let contact = document.querySelector('#contact');
 
 
+window.onload = function() {
+  if (window.innerWidth > 1200) {
+    sectionContHome.classList.remove('hidden');
+    sectionContAbout.classList.add('hidden');
+    sectionContProjects.classList.add('hidden');
+    sectionContContact.classList.add('hidden');
+  } else if (window.innerWidth < 1200) {
+    sectionContHome.classList.remove('hidden');
+    sectionContAbout.classList.remove('hidden');
+    sectionContProjects.classList.remove('hidden');
+    sectionContContact.classList.remove('hidden');
+  }
+};
+
 home.addEventListener ('click', function() {
   if (sectionContHome.classList.contains('hidden')) {
   sectionContHome.classList.remove('hidden');
